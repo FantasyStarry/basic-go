a1a9a816c8b373e433369aa4d4f90fa80db87713
FantasyStarry <spongzi@outlook.com>
2024-04-01 16:18:24 +0800

    解决gin跨域问题

--- webook/main.go
+++ webook/main.go
@@ -2,11 +2,28 @@ package main
 
 import (
 	"basic-go/webook/internal/web"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"strings"
+	"time"
 )
 
 func main() {
 	server := gin.Default()
+	server.Use(cors.New(cors.Config{
+		//AllowOrigins:     []string{"http://localhost:3000"},
+		//AllowMethods:     []string{"POST", "GET", "PUT", "DELETE"},
+		AllowHeaders:     []string{"Content-Type"},
+		AllowCredentials: true, // 是否允许携带 cookie
+		AllowOriginFunc: func(origin string) bool {
+			if strings.HasPrefix(origin, "http://localhost") {
+				//你的开发环境
+				return true
+			}
+			return strings.Contains(origin, "yourcompany.com")
+		},
+		MaxAge: 12 * time.Hour,
+	}))
 	u := web.NewUserHandler()
 	u.RegisterUsersRoutes(server)
 	server.Run(":8080")
