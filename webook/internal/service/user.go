bed369cf41fc5b9a4c11f2cf61f0d62a59bcc38b
FantasyStarry <spongzi@outlook.com>
2024-04-04 09:04:17 +0800

    加密

--- webook/internal/service/user.go
+++ webook/internal/service/user.go
@@ -4,6 +4,7 @@ import (
 	"basic-go/webook/internal/domain"
 	"basic-go/webook/internal/repository"
 	"context"
+	"golang.org/x/crypto/bcrypt"
 )
 
 type UserService struct {
@@ -19,5 +20,10 @@ func NewUserService(repo *repository.UserRepository) *UserService {
 func (svc *UserService) SignUp(ctx context.Context, u domain.User) error {
 	// 考虑加密存放的问题
 	// 然后就是存起来
+	hash, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+	if err != nil {
+		return err
+	}
+	u.Password = string(hash)
 	return svc.repo.Create(ctx, u)
 }
